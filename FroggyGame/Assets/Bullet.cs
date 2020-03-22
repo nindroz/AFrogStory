5485d9813fa3c98fcd151ec50eee758bf2e390cc
Hitesh <mantha.hitesh@gmail.com>
2020-03-22 13:02:49 -0700

    added shoting

---
+++ FroggyGame/Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //moves bullet
+        rb.velocity = transform.right * Speed * testCharMovementScript.direction;
+    }
+    //chekcs for bullet hits
+    void OnTriggerEnter2D( Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+   
+}
---
+++ FroggyGame/Assets/Scripts/turningScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turningScript : MonoBehaviour
+{
+
+
+    public GameObject firePoint;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testCharMovementScript.direction == 1)
+        {
+            firePoint.transform.position = new Vector2(player.transform.position.x + 3,firePoint.transform.position.y);
+        }
+        else
+        {
+            firePoint.transform.position = new Vector2(player.transform.position.x-3, firePoint.transform.position.y);
+        }
+        if (player)
+        {
+
+        } 
+    }
+}
