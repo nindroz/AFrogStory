08388252c7a64ef0b262308c1d58e43de5e2a83b
sarah <sjwang0628@gmail.com>
2020-03-31 16:19:15 -0700

    horizontal enemy shoots stuff at u

---
+++ FroggyGame/Assets/Scripts/horizontalenemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horizontalenemy : MonoBehaviour
+{
+    // i have no idea whats going on i just following tutorial :(
+    public float speed;
+    public float distance = 2f;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime); // move ground detection gameobject to the right
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance); //raycast shooting from gameobject down
+        if (groundInfo.collider == false) // if ray hasn't collided w anything 
+        {
+            if (movingRight == true) 
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0); //turn left
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0); //if moving left and ray doesn't detect anything, turn right
+                movingRight = true;
+            }
+
+        }
+    }
+}
---
+++ FroggyGame/Assets/Scripts/horizontalenemybullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horizontalenemybullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, Vector2.right, 2f); //raycast shooting from gameobject right
+        if (hitInfo.collider == true)
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            //createsa bullet from the prefab
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+    }
+}
